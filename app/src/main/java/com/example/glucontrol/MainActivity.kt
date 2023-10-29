c8ff5f94e67887eb166fd6bcd4c48378885814d1
Lazaro <lheredia@cash.com.uy>
2023-10-29 17:01:23 -0300

    creando tablas de base datos sqlite para registro de valores

--- app/src/main/java/com/example/glucontrol/MainActivity.kt
+++ app/src/main/java/com/example/glucontrol/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -19,9 +20,29 @@ class MainActivity : AppCompatActivity() {
         myDB.execSQL(
             "CREATE TABLE IF NOT EXISTS user (name VARCHAR(200))"
         );
+        myDB.execSQL(
+            "CREATE TABLE IF NOT EXISTS control (fecha VARCHAR(200), valor INTEGER)"
+        );
+        //Realizo consulta para saber si ya esta registrado
+        val myCursor = myDB.rawQuery("select name from user", null)
+        var name:String = ""
+        while (myCursor.moveToNext()) {
+            name = myCursor.getString(0)
+            break
+        }
+        myCursor.close();
+        //Capturamos el componente de Texto
+        val editRegistrado=findViewById<EditText>(R.id.edtTextoRegistro)
+        editRegistrado.setText(name)
+
         //Recupero el boton de registro y el texto escrito
         val btnRegistro = findViewById<Button>(R.id.btnRegistrar)
         val edtTexto = findViewById<EditText>(R.id.edtTextoRegistro)
+
+        //Si ya esta registrado cambio el texto del boton a entrar
+        if (name != "") {
+            btnRegistro.setText("Entrar")
+        }
 
         btnRegistro.setOnClickListener {
             //Creo el registro de datos
